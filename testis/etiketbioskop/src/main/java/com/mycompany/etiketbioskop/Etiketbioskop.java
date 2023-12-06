b62655ad89d2460c43c1b7042eb829dde0b6cc4a
lokithemischiefgod <152399065+lokithemischiefgod@users.noreply.github.com>
2023-12-06 14:36:06 +0800

    Add files via upload

---
+++ testis/etiketbioskop/src/main/java/com/mycompany/etiketbioskop/Etiketbioskop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.etiketbioskop;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Etiketbioskop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
